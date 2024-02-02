faa4080655cea536d9e10cd0815d871ff299e239
Bogdan Babic <babicbogdan18@yahoo.com>
2024-02-02 00:03:59 -0600

    Implemented the TopicList with proper information from the database, can be accessed properly from Navbar from now on

--- Blogging/Controllers/TopicController.cs
+++ Blogging/Controllers/TopicController.cs
@@ -6,6 +6,18 @@ namespace Blogging.Controllers
 {
     public class TopicController : Controller
     {
+        public readonly ITopicRepository _topicRepository;
+        public readonly IPostRepository _postRepository;
+        public TopicController(ITopicRepository topicRepository, IPostRepository postRepository)
+        {
+            _topicRepository = topicRepository;
+            _postRepository = postRepository;
+        }
 
+        public IActionResult TopicList()
+        {
+            IEnumerable<Topic> topics = _topicRepository.GetAllTopics();
+            return View(topics);
+        }
     }
 }
---
+++ Blogging/ViewModels/TopicListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Blogging.Models;
+
+namespace Blogging.ViewModels
+{
+    public class TopicListViewModel
+    {
+        public IEnumerable<Topic> Topics { get; set; }
+        public TopicListViewModel(IEnumerable<Topic> topics)
+        {
+            Topics = topics;
+        }
+    }
+}
